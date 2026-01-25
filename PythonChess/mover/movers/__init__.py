87ee51a413dcb94c5057703fa9e5f7bac227f1ae
mbaas038 <martijn.baas@ordina.nl>
2026-01-25 13:59:17 +0100

    Move module imports in init to all

--- PythonChess/mover/movers/__init__.py
+++ PythonChess/mover/movers/__init__.py
@@ -1,2 +1,10 @@
+from mover.movers.bishop_mover import BishopMover
 from mover.movers.knight_mover import KnightMover
 from mover.movers.pawn_mover import PawnMover
+
+
+__all__ = [
+    "BishopMover",
+    "KnightMover",
+    "PawnMover",
+]
--- PythonChess/piece/pieces/__init__.py
+++ PythonChess/piece/pieces/__init__.py
@@ -1,2 +1,10 @@
+from piece.pieces.bishop import Bishop
 from piece.pieces.knight import Knight
 from piece.pieces.pawn import Pawn
+
+
+__all__ = [
+    "Bishop",
+    "Knight",
+    "Pawn",
+]
